debbf8d8092cbf9d4b595e43fc9b05593fab8a97
Coop <coop.coding@gmail.com>
2016-02-25 14:38:23 +1100

    Added crash event hadler

--- renderer.js
+++ renderer.js
@@ -6,11 +6,19 @@ var ipcRenderer = electron.ipcRenderer
 module.exports = function(){
 
   ipcRenderer.on('start', event =>{
+
     console.log('start')
+
     var webview = document.createElement('webview')
+
     webview.addEventListener('gpu-crashed', event =>{
       console.log(`webview: gpu-crashed`)
     })
+
+    webview.addEventListener('crashed', event =>{
+      console.log(`webview: crashed`)
+    })
+
     webview.addEventListener('did-finish-load', event =>{
       console.log(`webview: did-finish-load`)
     })
@@ -21,8 +29,8 @@ module.exports = function(){
     })
 
     //webview.setAttribute('src', 'http://google.com')
-    webview.setAttribute('src', 'chrome://gpucrash')
-    //webview.setAttribute('src', 'chrome://crash')
+    //webview.setAttribute('src', 'chrome://gpucrash')
+    webview.setAttribute('src', 'chrome://crash')
     document.body.appendChild(webview)
 
   })
